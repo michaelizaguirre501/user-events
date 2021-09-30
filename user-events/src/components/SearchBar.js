79e916c42a5b87bc80ee9e1271ff5d0617fdf404
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-29 20:17:43 -0400

    search bar set up onsubmit prop passed from app to search bar

--- user-events/src/components/SearchBar.js
+++ user-events/src/components/SearchBar.js
@@ -3,10 +3,15 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="searchBar ui segment">
-        <form className="searchBarForm ui form" action="">
+        <form className="searchBarForm ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
