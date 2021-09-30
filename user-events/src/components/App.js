66d08fd998dc138c6a80b1c156a209787b94d004
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-29 21:40:24 -0400

    fetch works need to parse results

--- user-events/src/components/App.js
+++ user-events/src/components/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const results = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID l_Qlp27UJu1it51J7vz4kF7nRnJF2GlRpnXNGIdlqOA",
+      },
+    });
+    let array = results.data.results.map((x) => x.urls.regular);
+    return array;
   }
   render() {
     return (
       <div className="ui container mt" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {console.log("ok")}
+        {/*here */}
       </div>
     );
   }
